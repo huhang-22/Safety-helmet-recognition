eaaf5d64b6f00b85f0a6552557be62761687a61e
huhang-22 <3137896041@qq.com>
2026-01-17 13:34:12 +0800

    first commit

--- train.py
+++ train.py
@@ -2,16 +2,16 @@ from ultralytics import YOLO
 import os
 
 def main():
-    print("=== 开始YOLOv8安全检测模型训练 ===")
+    print("开始YOLOv8安全检测模型训练")
     
     model = YOLO('yolov8n.pt')
 
     results = model.train(
         data='E:/bishe/trained/data.yaml',  
-        epochs=50,              # 训练轮数
+        epochs=30,              # 训练轮数
         imgsz=640,              # 图像尺寸
         batch=8,                # 批大小
-        name='safety_detection_v3',  # 实验名称
+        name='safety_detection_v2',  # 实验名称
         patience=10,            # 早停耐心值
         save=True,              # 保存模型
         device=0            
